f13f9af417e5fdaa9b2f33f3d359942b33576354
EngelAlexey <alexhmanzanares@gmail.com>
2025-11-29 17:43:15 -0600

    Fixing attendances put

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -6,10 +6,18 @@ exports.getAll = (req, res) => {
     res.json(data);
 };
 
+exports.getById = (req, res) => {
+    const data = fileHelper.readJson(FILE_NAME);
+    const result = data.find(a => String(a.idAttendance) === String(req.params.id));
+    result ? res.json(result) : res.status(404).json({message: "Not found"});
+};
+
 exports.create = (req, res) => {
     const data = fileHelper.readJson(FILE_NAME);
     const newAttendance = req.body;
+    
     if(!newAttendance.idAttendance) newAttendance.idAttendance = Date.now().toString();
+    
     data.push(newAttendance);
     fileHelper.writeJson(FILE_NAME, data);
     res.status(201).json(newAttendance);
@@ -18,26 +26,28 @@ exports.create = (req, res) => {
 exports.update = (req, res) => {
     let data = fileHelper.readJson(FILE_NAME);
     const updated = req.body;
-    const index = data.findIndex(a => a.idAttendance === updated.idAttendance);
+
+    const index = data.findIndex(a => String(a.idAttendance) === String(updated.idAttendance));
     
     if (index !== -1) {
         data[index] = updated;
         fileHelper.writeJson(FILE_NAME, data);
         res.json(updated);
     } else {
-        res.status(404).json({ message: "Attendance not found" });
+        res.status(404).json({ message: "Attendance not found for update" });
     }
 };
 
 exports.delete = (req, res) => {
     let data = fileHelper.readJson(FILE_NAME);
     const initialLength = data.length;
-    data = data.filter(a => a.idAttendance !== req.params.id);
+
+    data = data.filter(a => String(a.idAttendance) !== String(req.params.id));
 
     if (data.length < initialLength) {
         fileHelper.writeJson(FILE_NAME, data);
         res.json({ message: "Attendance deleted" });
     } else {
-        res.status(404).json({ message: "Attendance not found" });
+        res.status(404).json({ message: "Attendance not found for delete" });
     }
 };
